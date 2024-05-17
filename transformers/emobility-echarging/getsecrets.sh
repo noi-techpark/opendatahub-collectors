56f88c9e301866eda055eaf5a09edcc3e8f4ad5b
Clemens Zagler <c.zagler@noi.bz.it>
2024-05-17 15:31:11 +0200

    echarging transformer: add local debugging tools

--- transformers/emobility-echarging/getsecrets.sh
+++ transformers/emobility-echarging/getsecrets.sh
@@ -2,3 +2,6 @@
 echo Rabbit URL: `kubectl get secret -n collector rabbitmq-svcbind -o jsonpath='{.data.uri}' | base64 -d`
 echo Mongo URL: `kubectl get secret -n collector mongodb-collector-svcbind -o jsonpath='{.data.uri}' | base64 -d`
 echo Oauth client secret: `kubectl get secret -n collector oauth-collector -o jsonpath='{.data.clientSecret}' | base64 -d`
+echo Oauth client id: `kubectl get secret -n collector oauth-collector -o jsonpath='{.data.clientId}' | base64 -d`
+echo Oauth auth url: `kubectl get secret -n collector oauth-collector -o jsonpath='{.data.authorizationUri}' | base64 -d`
+echo Oauth token url: `kubectl get secret -n collector oauth-collector -o jsonpath='{.data.tokenUri}' | base64 -d`
