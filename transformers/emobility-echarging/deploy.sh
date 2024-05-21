f006e7ecee6737284aecd17b8502e29011cc767d
Clemens Zagler <c.zagler@noi.bz.it>
2024-05-21 10:06:49 +0200

    upgrade echarging transformers to use secrets

--- transformers/emobility-echarging/deploy.sh
+++ transformers/emobility-echarging/deploy.sh
@@ -5,6 +5,6 @@ RELEASETIME=`date +%s`
 && mvn clean compile package \
 && docker build -t ghcr.io/noi-techpark/opendatahub-collectors/emobility-echarging:0.1.0 . -f infrastructure/docker/Dockerfile \
 && docker image push ghcr.io/noi-techpark/opendatahub-collectors/emobility-echarging:0.1.0 \
-&& helm upgrade --namespace collector --install tr-echarging-alperia ../../helm/generic-collector --values infrastructure/helm/alperia_values.yaml --set-string podAnnotations.releaseTime=$RELEASETIME \
-&& helm upgrade --namespace collector --install tr-echarging-driwe ../../helm/generic-collector --values infrastructure/helm/driwe_values.yaml --set-string podAnnotations.releaseTime=$RELEASETIME \
-&& helm upgrade --namespace collector --install tr-echarging-route220 ../../helm/generic-collector --values infrastructure/helm/route220_values.yaml --set-string podAnnotations.releaseTime=$RELEASETIME
+&& helm upgrade --namespace collector --install tr-echarging-alperia ../../helm/generic-collector --values infrastructure/helm/alperia.yaml --set-string podAnnotations.releaseTime=$RELEASETIME \
+&& helm upgrade --namespace collector --install tr-echarging-driwe ../../helm/generic-collector --values infrastructure/helm/driwe.yaml --set-string podAnnotations.releaseTime=$RELEASETIME \
+#&& helm upgrade --namespace collector --install tr-echarging-route220 ../../helm/generic-collector --values infrastructure/helm/route220_values.yaml --set-string podAnnotations.releaseTime=$RELEASETIME
