b49fadd5c05b788d13f83fac595baa296f94dbfe
Clemens Zagler <c.zagler@noi.bz.it>
2024-08-06 11:43:15 +0200

    cleanup data collector lib to necessary and used features, don't propagate internal camel headers

--- collectors/lib/ingress-mq/src/main/java/com/opendatahub/collector/lib/ingress/mq/MsgRoute.java
+++ collectors/lib/ingress-mq/src/main/java/com/opendatahub/collector/lib/ingress/mq/MsgRoute.java
@@ -38,23 +38,6 @@ public class MsgRoute extends RouteBuilder {
             .routeId("to-odh-ingress-route")
             // Possibility to set providers from header, overriding the configured one. Use in case you need more than one provider
             .process(exchange -> WrapperProcessor.process(exchange, exchange.getMessage().getHeader(HEADER_PROVIDER, provider, String.class)))
-            // .to("file:bar?doneFileName=done")
-            .choice()
-            // forward to fastline
-            .when(header("fastline").isEqualTo(true))
-            // we handle the request as invalid and forward the encapsulated payload to
-            // whatever mechanism we want to use to store malformed data
-            .to(rabbitMQConfig.getRabbitMQFastlineConnectionString())
-            .end()
-            .choice()
-            // if the payload is not a valid json
-            .when(header("failed").isEqualTo(true))
-            // we handle the request as invalid and forward the encapsulated payload to
-            // whatever mechanism we want to use to store malformed data
-            .to(rabbitMQConfig.getRabbitMQIngressDeadletterTo())
-            .otherwise()
-            // otherwise we forward the encapsulated message to the
-            // internal queue waiting to be written in rawDataTable
             .to(rabbitMQConfig.getRabbitMQIngressTo());
     }
 }
--- collectors/lib/ingress-mq/src/main/java/com/opendatahub/collector/lib/ingress/mq/RabbitMQConnection.java
+++ collectors/lib/ingress-mq/src/main/java/com/opendatahub/collector/lib/ingress/mq/RabbitMQConnection.java
@@ -18,12 +18,7 @@ public class RabbitMQConnection {
     @Value("${ingress.rabbitmq.clientname}")
     String clientname;
     
-    static final String RABBITMQ_INGRESS_QUEUE = "ingress-q";
     static final String RABBITMQ_INGRESS_EXCHANGE = "ingress";
-    static final String RABBITMQ_INGRESS_DEADLETTER_QUEUE = "ingress-dl-q";
-    static final String RABBITMQ_INGRESS_DEADLETTER_EXCHANGE = "ingress-dl";
-    static final String RABBITMQ_FASTLINE_EXCHANGE = "fastline";
-    
     private static final String CONNECTION_FACTORY = "odh-ingress";
 
     @Bean(CONNECTION_FACTORY)
@@ -35,21 +30,8 @@ public class RabbitMQConnection {
     }
     
     public String getRabbitMQIngressTo() {
-        return String.format("spring-rabbitmq:%s?connectionFactory=#bean:%s&queues=%s&exchangePattern=InOnly&exchangeType=fanout&acknowledgeMode=AUTO",
+        return String.format("spring-rabbitmq:%s?connectionFactory=#bean:%s&exchangePattern=InOnly&exchangeType=fanout&acknowledgeMode=AUTO",
                 RABBITMQ_INGRESS_EXCHANGE,
-                CONNECTION_FACTORY,
-                RABBITMQ_INGRESS_QUEUE);
-    }
-
-    public String getRabbitMQIngressDeadletterTo() {
-        return String.format("spring-rabbitmq:%s?queues=%s&exchangePattern=InOnly&exchangeType=fanout&acknowledgeMode=AUTO",
-                RABBITMQ_INGRESS_DEADLETTER_EXCHANGE,
-                RABBITMQ_INGRESS_DEADLETTER_QUEUE);
-    }
-
-    public String getRabbitMQFastlineConnectionString() {
-        final StringBuilder uri = new StringBuilder(String.format("spring-rabbitmq:%s?exchangePattern=InOnly&exchangeType=topic&acknowledgeMode=AUTO",
-                RABBITMQ_FASTLINE_EXCHANGE));
-        return uri.toString();
+                CONNECTION_FACTORY);
     }
 }
