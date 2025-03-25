e8e51d51577d3c5746d8c198ca4b7b91f251b7d4
Matteo Roggia <matteo.roggia@gmail.com>
2025-03-25 09:32:43 +0100

    chore(multi-rest-poller-parking): prod ci/cd

---
+++ collectors/multi-rest-poller/src/main_test.go
@@ -0,0 +1,15 @@
+// SPDX-FileCopyrightText: 2024 NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package main
+
+// TODO the below test is an example of jsonpath dereferencing a nil pointer and panicing
+// func TestEmptyResposeJsonPath(t *testing.T) {
+
+// 	val, err := jsonpath.JsonPathLookup(map[string]any{
+// 		"FreePlaces": nil,
+// 	}, "$.FreePlaces[*]")
+// 	require.Nil(t, err)
+// 	require.Nil(t, val)
+// }
