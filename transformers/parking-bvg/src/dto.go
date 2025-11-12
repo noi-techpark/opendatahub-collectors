2e6467fa0f3f73774a30c09dd4890d9b47f32c52
Matteo Roggia <matteo.roggia@gmail.com>
2025-11-12 16:03:10 +0100

    feat(dc-api-crawler-parking-bvg): implemented collector feat(tr-parking-bvg): implemented transformer

---
+++ transformers/parking-bvg/src/dto.go
@@ -0,0 +1,111 @@
+// SPDX-FileCopyrightText: NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package main
+
+import "time"
+
+// CountingAreaList is the top-level structure, a slice of CountingArea
+type CountingAreaList []CountingArea
+
+// CountingArea represents a single element in the top-level array
+type CountingArea struct {
+	AppearanceParams AppearanceParams `json:"appearance_params"`
+	Counts           Counts           `json:"counts"`
+	Created          time.Time        `json:"created"`
+	ID               string           `json:"id"`
+	LastResetTime    *time.Time       `json:"last_reset_time"` // Use *time.Time for null or date string
+	MultiSg          bool             `json:"multi_sg"`
+	Name             string           `json:"name"`
+	ResetDayOfWeek   *string          `json:"reset_day_of_week"` // Use *string for null or string
+	ResetMode        string           `json:"reset_mode"`
+	ResetTimeOfDay   *string          `json:"reset_time_of_day"` // Use *string for null or string
+	ResetTimezone    *string          `json:"reset_timezone"`    // Use *string for null or string
+	SiteID           string           `json:"site_id"`
+	Type             string           `json:"type"`
+	Where            Where            `json:"where"`
+}
+
+// AppearanceParams contains parameters for object appearance
+type AppearanceParams struct {
+	AoiOverlapDegree             float64  `json:"aoi_overlap_degree"`
+	BottomColours                *string  `json:"bottom_colours"` // null
+	ClassCategories              []string `json:"class_categories"`
+	DissimilarEmbeddings         *string  `json:"dissimilar_embeddings"` // null
+	DissimilarityThreshold       float64  `json:"dissimilarity_threshold"`
+	EnableObjectMotionEstimation bool     `json:"enableObjectMotionEstimation"`
+	ExcludeStationaryObjects     bool     `json:"excludeStationaryObjects"`
+	FaceWatchlistIds             []string `json:"face_watchlist_ids"`
+	HasFace                      bool     `json:"has_face"`
+	HasLicensePlate              bool     `json:"has_license_plate"`
+	LicensePlateClauses          *string  `json:"license_plate_clauses"` // null
+	MinimumAttributeScore        int      `json:"minimum_attribute_score"`
+	MinimumObjectSize            int      `json:"minimum_object_size"`
+	MinimumObjectSizeOverride    *string  `json:"minimum_object_size_override"` // null
+	PerAttributeThresholds       *string  `json:"per_attribute_thresholds"`     // null
+	PersonAccessories            *string  `json:"person_accessories"`           // null
+	PersonBodyStates             *string  `json:"person_body_states"`           // null
+	SimilarEmbeddings            *string  `json:"similar_embeddings"`           // null
+	SimilarFaceEmbeddings        *string  `json:"similar_face_embeddings"`      // null
+	SimilarityPlates             *string  `json:"similarity_plates"`            // null
+	SimilarityThreshold          float64  `json:"similarity_threshold"`
+	TopColours                   *string  `json:"top_colours"` // null
+	VehicleColours               []string `json:"vehicle_colours"`
+	VehicleTypes                 []int    `json:"vehicle_types"`
+}
+
+// Counts contains the totals array
+type Counts struct {
+	Totals []Total `json:"totals"`
+}
+
+// Total contains the aggregated counting data
+type Total struct {
+	CountDetails    CountDetails `json:"countDetails"`
+	CountDetailsIn  CountDetails `json:"countDetailsIn"`
+	CountDetailsOut CountDetails `json:"countDetailsOut"`
+	CountPerson     int          `json:"countPerson"`
+	CountPersonIn   int          `json:"countPersonIn"`
+	CountPersonMin  int          `json:"countPersonMin"`
+	CountPersonOut  int          `json:"countPersonOut"`
+	CountVehicle    int          `json:"countVehicle"`
+	CountVehicleIn  int          `json:"countVehicleIn"`
+	CountVehicleMin int          `json:"countVehicleMin"`
+	CountVehicleOut int          `json:"countVehicleOut"`
+	Received        int64        `json:"received"` // Timestamp in milliseconds
+}
+
+// CountDetails holds vehicle type counts, which can be an empty object
+type CountDetails struct {
+	VehicleTypeCounts map[string]int `json:"vehicleTypeCounts"` // Use map[string]int for the dynamic keys
+}
+
+// Where specifies the location and devices
+type Where struct {
+	DeviceGroups  *string        `json:"device_groups"` // null
+	DeviceSources []DeviceSource `json:"device_sources"`
+}
+
+// DeviceSource contains details about the source device
+type DeviceSource struct {
+	DeviceID               string   `json:"device_id"`
+	Loi                    []string `json:"loi"`                       // Array is empty in sample, assuming []string
+	ObjectTypeSizeSettings *string  `json:"object_type_size_settings"` // null
+	Paoi                   []Paoi   `json:"paoi"`
+	ServerGroupID          string   `json:"server_group_id"`
+	Source                 string   `json:"source"`
+	Version                int      `json:"version"`
+	WithinGroup            bool     `json:"within_group"`
+}
+
+// Paoi (Polygon of Interest) contains a list of points
+type Paoi struct {
+	Points []Point `json:"points"`
+}
+
+// Point represents a coordinate
+type Point struct {
+	X float64 `json:"X"`
+	Y float64 `json:"Y"`
+}
---
+++ transformers/parking-bvg/src/main_test.go
@@ -0,0 +1,46 @@
+// SPDX-FileCopyrightText: 2024 NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+package main
+
+import (
+	"context"
+	"testing"
+	"time"
+
+	"github.com/noi-techpark/go-bdp-client/bdplib"
+	"github.com/noi-techpark/go-bdp-client/bdpmock"
+	"github.com/noi-techpark/opendatahub-go-sdk/ingest/rdb"
+	"github.com/noi-techpark/opendatahub-go-sdk/testsuite"
+	"github.com/stretchr/testify/require"
+)
+
+func Test1(t *testing.T) {
+	var in = CountingAreaList{}
+	err := testsuite.LoadInputData(&in, "../testdata/in.json")
+	require.Nil(t, err)
+
+	timestamp, err := time.Parse("2006-01-02", "2025-01-01")
+	require.Nil(t, err)
+
+	raw := rdb.Raw[CountingAreaList]{
+		Rawdata:   in,
+		Timestamp: timestamp,
+	}
+
+	var out = bdpmock.BdpMockCalls{}
+	err = testsuite.LoadOutput(&out, "../testdata/out.json")
+	require.Nil(t, err)
+
+	b := bdpmock.MockFromEnv(bdplib.BdpEnv{})
+
+	err = Transform(context.TODO(), b, &raw)
+	require.Nil(t, err)
+
+	mock := b.(*bdpmock.BdpMock)
+
+	req := mock.Requests()
+	// testsuite.WriteOutput(req, "../testdata/out.json")
+	bdpmock.CompareBdpMockCalls(t, out, req)
+}
