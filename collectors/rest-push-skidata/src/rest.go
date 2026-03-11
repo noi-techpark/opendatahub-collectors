d7275492de68ce7d34f75c3d974b5144d94ff57d
Matteo Roggia <matteo.roggia@gmail.com>
2026-03-11 16:09:06 +0100

    added more missing heltchchecks

--- collectors/rest-push-skidata/src/rest.go
+++ collectors/rest-push-skidata/src/rest.go
@@ -37,6 +37,12 @@ func serve(inputCh chan<- dc.Input[PushPayload]) {
 	e.HEAD("/push/skidata/parking-stations/health", func(c echo.Context) error {
 		return c.NoContent(http.StatusOK)
 	})
+	e.GET("/push/skidata/parking-stations/v1/health", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
+	e.HEAD("/push/skidata/parking-stations/v1/health", func(c echo.Context) error {
+		return c.NoContent(http.StatusOK)
+	})
 
 	skidata := e.Group("/push/skidata/parking-stations",
 		middleware.BasicAuth(validateInbound))
