46950a9ecbcf544ff87eb3ccf7e60a66c171bec7
kaiozwald <a.abuhassan@noi.bz.it>
2026-06-22 14:42:32 +0200

    feat: setup xml api crawler as well as webcam-feratel logic and enhancements to other webcamdata

---
+++ transformers/webcam-feratel/src/main_test.go
@@ -0,0 +1,74 @@
+// SPDX-FileCopyrightText: 2026 NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: CC0-1.0
+
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/noi-techpark/opendatahub-go-sdk/clib"
+	"github.com/noi-techpark/opendatahub-go-sdk/clib/clibmock"
+	"github.com/noi-techpark/opendatahub-go-sdk/ingest/rdb"
+	"github.com/noi-techpark/opendatahub-go-sdk/testsuite"
+
+	odhmodel "github.com/noi-techpark/opendatahub-collectors/transformers/webcam-feratel/odh-content-model"
+)
+
+func Test_Transform_Snapshot(t *testing.T) {
+	fixedNow := time.Date(2026, 1, 15, 10, 0, 0, 0, time.UTC)
+	timeNow = func() time.Time { return fixedNow }
+	defer func() { timeNow = time.Now }()
+
+	for _, suffix := range []string{"", "_full"} {
+		t.Run("Snapshot"+suffix, func(t *testing.T) {
+			mock := clibmock.NewContentMock()
+			contentClient = mock
+			webcamCache = clib.NewCache[odhmodel.WebcamInfo]()
+
+			// Read wrapped string from in.json or in_full.json
+			inFile := "../testdata/in" + suffix + ".json"
+			b, err := os.ReadFile(inFile)
+			if err != nil {
+				t.Fatalf("failed to read %s: %v", inFile, err)
+			}
+
+			var rawXMLString string
+			err = json.Unmarshal(b, &rawXMLString)
+			if err != nil {
+				t.Fatalf("failed to unmarshal %s: %v", inFile, err)
+			}
+
+			r := &rdb.Raw[string]{
+				Rawdata:   rawXMLString,
+				Timestamp: fixedNow,
+			}
+
+			err = Transform(context.TODO(), r)
+			if err != nil {
+				t.Fatalf("Transform failed: %v", err)
+			}
+
+			calls := mock.Calls()
+
+			var expected clibmock.MockCalls
+			outFile := "../testdata/out" + suffix + ".json"
+			err = testsuite.LoadOutput(&expected, outFile)
+			if err != nil {
+				t.Logf("No snapshot found, generating %s", outFile)
+				err = testsuite.WriteOutput(calls, outFile)
+				if err != nil {
+					t.Fatalf("failed to write snapshot: %v", err)
+				}
+				t.Log("Snapshot generated. Re-run the test to validate.")
+				return
+			}
+
+			clibmock.CompareMockCalls(t, expected, calls)
+		})
+	}
+}
---
+++ transformers/webcam-feratel/src/odh-content-model/webcam.go
@@ -0,0 +1,85 @@
+// SPDX-FileCopyrightText: 2025 NOI Techpark <digital@noi.bz.it>
+//
+// SPDX-License-Identifier: CC0-1.0
+
+package odhmodel
+
+import "time"
+
+// --- ODH Target Schema (WebcamInfo) ---
+
+type WebcamInfo struct {
+	Id               string                       `json:"Id"`
+	WebcamId         string                       `json:"WebcamId,omitempty"`
+	Source           string                       `json:"Source"`
+	Active           bool                         `json:"Active"`
+	SmgActive        bool                         `json:"SmgActive"`
+	OdhActive        bool                         `json:"OdhActive"`
+	WebCamProperties WebCamProperties             `json:"WebCamProperties"`
+	LastChange       time.Time                    `json:"LastChange,omitempty"`
+	Shortname        string                       `json:"Shortname,omitempty"`
+	Detail           map[string]Detail            `json:"Detail,omitempty"`
+	GpsInfo          []GpsInfo                    `json:"GpsInfo,omitempty"`
+	ContactInfos     map[string]ContactInfo       `json:"ContactInfos,omitempty"`
+	ImageGallery     []ImageGallery               `json:"ImageGallery,omitempty"`
+	VideoItems       map[string][]VideoItem       `json:"VideoItems,omitempty"`
+	Mapping          map[string]map[string]string `json:"Mapping,omitempty"`
+	HasLanguage      []string                     `json:"HasLanguage,omitempty"`
+}
+
+type WebCamProperties struct {
+	ViewAngleDegree string `json:"ViewAngleDegree,omitempty"`
+	HasVR           bool   `json:"HasVR"`
+	ViewerType      string `json:"ViewerType,omitempty"`
+	WebcamUrl       string `json:"WebcamUrl,omitempty"`
+}
+
+type Detail struct {
+	Title     string   `json:"Title,omitempty"`
+	IntroText string   `json:"IntroText,omitempty"`
+	BaseText  string   `json:"BaseText,omitempty"`
+	Language  string   `json:"Language,omitempty"`
+	Keywords  []string `json:"Keywords,omitempty"`
+}
+
+type ContactInfo struct {
+	Region      string `json:"Region,omitempty"`
+	Language    string `json:"Language,omitempty"`
+	LogoUrl     string `json:"LogoUrl,omitempty"`
+	ZipCode     string `json:"ZipCode,omitempty"`
+	City        string `json:"City,omitempty"`
+	Area        string `json:"Area,omitempty"`
+	CountryCode string `json:"CountryCode,omitempty"`
+	CountryName string `json:"CountryName,omitempty"`
+	Url         string `json:"Url,omitempty"`
+}
+
+type GpsInfo struct {
+	Gpstype               string  `json:"Gpstype"`
+	Latitude              float64 `json:"Latitude"`
+	Longitude             float64 `json:"Longitude"`
+	Altitude              float64 `json:"Altitude,omitempty"`
+	AltitudeUnitofMeasure string  `json:"AltitudeUnitofMeasure,omitempty"`
+}
+
+type ImageGallery struct {
+	Width        int      `json:"Width,omitempty"`
+	Height       int      `json:"Height,omitempty"`
+	ImageName    string   `json:"ImageName,omitempty"`
+	ImageUrl     string   `json:"ImageUrl,omitempty"`
+	ImageSource  string   `json:"ImageSource,omitempty"`
+	IsInGallery  bool     `json:"IsInGallery"`
+	ImageTags    []string `json:"ImageTags,omitempty"`
+	ListPosition int      `json:"ListPosition"`
+}
+
+type VideoItem struct {
+	Url             string  `json:"Url,omitempty"`
+	StreamingSource string  `json:"StreamingSource,omitempty"`
+	Active          bool    `json:"Active"`
+	Resolution      int     `json:"Resolution,omitempty"`
+	Definition      string  `json:"Definition,omitempty"`
+	Bitrate         int     `json:"Bitrate,omitempty"`
+	Duration        float64 `json:"Duration,omitempty"`
+	VideoType       string  `json:"VideoType,omitempty"`
+}
